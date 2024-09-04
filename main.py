194b6d0113b0aab207a6a98bd18395f02424ec72
trost1142 <tdawg.miller1142@gmail.com>
2024-09-04 17:29:20 -0600

    Adding starter files

---
+++ main.py
@@ -0,0 +1,10 @@
+from constants import *
+import pygame
+
+def main():
+  print("Starting asteroids!")
+  print("Screen width:", SCREEN_WIDTH)
+  print("Screen height:", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+  main()
