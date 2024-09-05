0bf84cd41613ab5d28c99a42ca5036eb090b2b52
trost1142 <tdawg.miller1142@gmail.com>
2024-09-05 16:59:43 -0600

    Blank screen with pygame

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
   print("Starting asteroids!")
   print("Screen width:", SCREEN_WIDTH)
   print("Screen height:", SCREEN_HEIGHT)
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          return
+    screen.fill(000)
+    pygame.display.flip()
 
 if __name__ == "__main__":
   main()
